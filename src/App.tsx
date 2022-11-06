0ae694dd4dd907bafa780d506fb8b0951a5bb0c8
Jungyub <jysong5005@gmail.com>
2022-11-06 18:50:16 +0900

    feat: relay 초기세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import React from 'react';
+import { RelayEnvironmentProvider } from 'react-relay/hooks';
+import RelayEnvironment from './relayEnvironment';
 import { SearchPage } from './pages/SearchPage';
 
+const { Suspense } = React;
+
 function App() {
-  return <SearchPage></SearchPage>;
+  return (
+    <RelayEnvironmentProvider environment={RelayEnvironment}>
+      <Suspense fallback={'Loading...'}>
+        <SearchPage />
+      </Suspense>
+    </RelayEnvironmentProvider>
+  );
 }
 
 export default App;
